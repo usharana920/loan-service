300c2d9665cdb4a423a50d8abcbf93ed131c0631
Usha Rana <usharana920@gmail.com>
2025-07-18 17:43:52 -0500

    initial commit

---
+++ src/main/java/org/usra/LoanService/LoanServiceApplication.java
@@ -0,0 +1,15 @@
+package org.usra.LoanService;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class LoanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanServiceApplication.class, args);
+	}
+
+}
