a2906a609161c381f68647f2b2a50c6b4b328c46
Usha Rana <usharana920@gmail.com>
2025-07-20 19:00:34 -0500

    support for swagger/ open api

--- src/main/java/org/usra/loanService/controller/CustomerController.java
+++ src/main/java/org/usra/loanService/controller/CustomerController.java
@@ -19,16 +19,23 @@ public class CustomerController {
 
     private final CustomerLoanService customerLoanService;
 
+    @GetMapping(value = "/welcome")
+    public String get(){
+        return "welcome";
+    }
+
     @PostMapping(value = "/apply", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<LoanApplicationResponse> apply(@RequestBody LoanFormRequest loanFormRequest){
-        log.info("inside apply controller. processing loan application");
+        log.info("CustomerController::apply - request for loan processing. ");
         String status = customerLoanService.processLoanApplication(loanFormRequest);
         return ResponseEntity.ok(LoanApplicationResponse.builder().status(status).build());
     }
 
     @GetMapping(value = "/getStatus/{quoteId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<LoanProcessedStatus> getLoanStatus(@PathVariable String quoteId){
+        log.info("CustomerController::getLoanStatus - request received for quoteId: {} ", quoteId);
         String status = customerLoanService.getLoanStatus(quoteId);
+        log.info("CustomerController::getLoanStatus::LoanProcessedStatus - response is: {} ", status);
         return ResponseEntity.ok(LoanProcessedStatus.builder().status(status).build());
     }
 }
